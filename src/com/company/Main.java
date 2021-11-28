b583b2368fc3693050fded1f6e981a227f5d5f3c
vyacheslav-sh <vs8629239@gmail.com>
2021-11-28 14:55:30 +0300

    рабочий код

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int ticketCost = 5010;
+    int rublesForMile = 20;
+    int bonusMiles = ticketCost/rublesForMile;
+    System.out.println(bonusMiles);
+    }
+}
